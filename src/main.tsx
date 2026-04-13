4e5890425c41653ecdfd3ef307e88c61058e5c98
MarioZitko <mariozitkovic2@gmail.com>
2026-04-13 22:54:36 +0200

    Map and select bugfix

--- src/main.tsx
+++ src/main.tsx
@@ -1,6 +1,7 @@
 import { StrictMode } from "react"
 import { createRoot } from "react-dom/client"
 import "./index.css"
+import "leaflet/dist/leaflet.css"
 import App from "./App.tsx"
 import L from "leaflet"
 import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png"
